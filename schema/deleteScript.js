faee7019353a6f42c77631cd581fe04ba74abd57
Shiva Kannan <s1kannan@ucsd.edu>
2017-05-11 19:02:20 -0700

    swags

--- schema/deleteScript.js
+++ schema/deleteScript.js
@@ -1,2 +1,9 @@
 const models = require('./schema');
-models.Message.find({}).remove().exec();
+models.Message.find({}, function(err, documents){
+  for(var i = 0; i < documents.length; i++){
+    documents[i]["Room"] = "york 2622";
+    documents[i].save();
+  }
+}).then(function(){
+  console.log("I am done!");
+});
